9713e5da4c96fd941f54b7fe7974868aca51c6ca
djarecka <djarecka@gmail.com>
2015-02-05 07:33:28 +0100

    wrocilam do wesji dzialajacej z dt-0.1 i bez podkrokow, z limiterem tylko podczas spinupu, ale zapuscilam dla 64 kropli

--- kid.py
+++ kid.py
@@ -132,8 +132,8 @@ def micro_step(it_diag, dt, size_z, size_x, th_ar, qv_ar, rhof_ar, rhoh_ar,
 
     # spinup period logic
     opts.sedi = opts.coal = timestep >= params["spinup"]
-    #if timestep >= params["spinup"]:
-    #  opts.RH_max = 1.5
+    if timestep >= params["spinup"]:
+      opts.RH_max = 1.5
     print "sedi, coal, RH_max", opts.sedi, opts.coal, opts.RH_max
 
     # superdroplets: all what have to be done within a timestep
